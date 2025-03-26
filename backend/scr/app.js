7ad8618ac57bf6b9dff8dc8b75fc9a539f53942e
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-03-27 04:56:08 +0530

    package removed

--- backend/scr/app.js
+++ backend/scr/app.js
@@ -11,25 +11,25 @@ const app = express();
 
 
 dbconnect()
-  .then(() => {
-      
-      const server = app.listen(process.env.PORT || 4000, () => {
-        console.log(`⚙️ server running on port ${process.env.PORT || 4000}`);
-      });
+    .then(() => {
 
-      app.on("error :" ,(error)=>{
-console.log(`server is not talking : ` , error);
-throw error ;
-      });
-      
+        const server = app.listen(process.env.PORT || 4000, () => {
+            console.log(`⚙️ server running on port ${process.env.PORT || 4000}`);
+        });
+
+        app.on("error :", (error) => {
+            console.log(`server is not talking : `, error);
+            throw error;
+        });
 
 
-  })
-  .catch((error) => {
-    console.error(`Error from app.js:::-> ${error}`);
-  });
+
+    })
+    .catch((error) => {
+        console.error(`Error from app.js:::-> ${error}`);
+    });
 
 app.get("/", (req, res) => {
-  res.send("hello Xet");
+    res.send("hello Xet");
 });
 
--- backend/scr/db/databseConnect.js
+++ backend/scr/db/databseConnect.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose"
+import dotenv , {config} from "dotenv"
+// dotenv.config({
+//     path:'./.env'
+// })
 const dbname = "srxit"
-
 const dbconnect = async ()=>{
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${dbname}`)
-        console.log(`MongoDB connectedsuccesfully:-  ${connection.connection.host}`);
+        const connectdb = await mongoose.connect(`${process.env.MONGODB_URI}/${dbname}`)
+        console.log(`MongoDB connectedsuccesfully:-  ${connectdb.connection.host}`);
     } catch (error) {
         console.log(`Error is:::-> ` ,error);
         process.exit(1)
