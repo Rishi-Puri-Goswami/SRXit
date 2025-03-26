9093e237516e60605cefa2163c640b7b19103284
Rishi-Puri-Goswami <rishigithub9@gmail.com>
2025-03-27 04:48:31 +0530

    finally db connected

--- backend/scr/app.js
+++ backend/scr/app.js
@@ -1,30 +1,35 @@
 import express from "express";
 import cors from "cors";
-import dbconnect from "./db/databseConnect";
+import dbconnect from "./db/databseConnect.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-// app.use(cors({
-//     origin: 
-// }))
+
+
 
 dbconnect()
-.then(()=>{
-    app.on("error : ", (error)=>{
-        console.log(`server is not talking:- ${error}`);
-        throw error
-    })
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`⚙️ server running on port ${process.env.PORT}`, );
-    })
+  .then(() => {
+      
+      const server = app.listen(process.env.PORT || 4000, () => {
+        console.log(`⚙️ server running on port ${process.env.PORT || 4000}`);
+      });
 
-})
-.catch((error)=>{
-    console.log(`Error from app.js:::-> ${error}`);
-    
-})
+      app.on("error :" ,(error)=>{
+console.log(`server is not talking : ` , error);
+throw error ;
+      });
+      
 
-app.get("/" , (req , res ) =>{
-    res.send("hello Xet");
-})
 
-app.listen(3000);
+  })
+  .catch((error) => {
+    console.error(`Error from app.js:::-> ${error}`);
+  });
+
+app.get("/", (req, res) => {
+  res.send("hello Xet");
+});
+
